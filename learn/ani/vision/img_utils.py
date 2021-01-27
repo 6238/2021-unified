46e48a9b0653189f79e4ab8e44849c8ed10ae248
AniAggarwal <antiani100@gmail.com>
2021-01-27 08:41:27 -0800

    Add more utils to img_utils, work with contours to find centeroids of shapes.

--- learn/ani/vision/img_utils.py
+++ learn/ani/vision/img_utils.py
@@ -18,9 +18,65 @@ class GeneralUtils:
                 cv2.drawContours(dest, [contour], -1, (255, 0, 0), 3)
             else:
                 cv2.drawContours(src, [contour], -1, (255, 0, 0), 3)
-        
+
         return contours
-        
+
+    def draw_bboxes(self, dest, rects, color=(0, 255, 0), thickness=2):
+        for rect in rects:
+            cv2.rectangle(
+                dest, (rect[0], rect[1]), (rect[2], rect[3]), color, thickness
+            )
+
+    def draw_circles(self, dest, circles, radius=5, color=(0, 255, 0), thickness=-1):
+        for circle in circles:
+            cv2.circle(dest, (circle[0], circle[1]), radius, color, thickness)
+
+    def get_contour_centers(self, contours):
+        centeriods = []
+        for contour in contours:
+            M = cv2.moments(contour)
+            center_x = int(M["m10"] / M["m00"])
+            center_y = int(M["m01"] / M["m00"])
+            centeriods.append(np.array([center_x, center_y]))
+        return centeriods
+
+    def get_bbox_centers(self, bboxes):
+        centeriods = []
+        for bbox in bboxes:
+            centeriods.append(
+                np.array([(bbox[0] + bbox[2]) // 2, (bbox[1] + bbox[3]) // 2])
+            )
+        return centeriods
+
+    def calculate_iou(self, bbox1, bbox2):
+        """Calculates IOU of given two bounding boxes in form of numpy array [x1, y1, x2, y2]"""
+        assert bbox1[0] <= bbox1[2]
+        assert bbox1[1] <= bbox1[3]
+        assert bbox2[0] <= bbox2[2]
+        assert bbox2[1] <= bbox2[3]
+
+        intersection_area = (bbox1[0] - bbox1[2] + 1) * (bbox1[3] - bbox1[1] + 1)
+        bbox1_area = (bbox1[2] - bbox1[0] + 1) * (bbox1[3] - bbox1[1] + 1)
+        bbox2_area = (bbox2[2] - bbox2[0] + 1) * (bbox2[3] - bbox2[1] + 1)
+
+        iou = intersection_area / float(bbox1_area + bbox2_area - intersection_area)
+        assert iou >= 0.0
+        assert iou <= 1.0
+        return iou
+
+    def generate_bboxes(self, contours):
+        bounding_rects = []
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            bounding_rects.append(np.array([x, y, x + w, y + h]))
+
+        return bounding_rects
+
+    def connect_similar_contours(self, contours):
+        rects = self.generate_bboxes(contours)
+        new_rects, weights = cv2.groupRectangles(rects, 1)
+        return new_rects, weights
+
 
 class ShapeDetector:
     def detect_shape(self, contour, num_sides):
@@ -38,12 +94,11 @@ class ShapeDetector:
         """Draws contours from given src on given dst (drawn on src if no dst specified) and returns said contours
         if detected contours have given number of sides."""
         contours = cv2.findContours(src, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = imutils.grab_contours(contours)
         for contour in contours:
             if self.detect_shape(contour, num_sides):
                 if dest is not None:
                     cv2.drawContours(dest, [contour], -1, (255, 0, 0), 3)
                 else:
                     cv2.drawContours(src, [contour], -1, (255, 0, 0), 3)
-    
+
         return contours
--- learn/ani/vision/shape_detection_img.py
+++ learn/ani/vision/shape_detection_img.py
@@ -9,10 +9,11 @@ from img_utils import GeneralUtils, ShapeDetector
 shape_detector = ShapeDetector()
 utils = GeneralUtils()
 
-img_path = Path("./target.jpg")
+img_path = Path("./target-imgs/mid-target.jpg")
 # img_path = Path("./2012-retroreflective-targets.jpg")
 img = cv2.imread(str(img_path))
 img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+target_img = img.copy()  # for drawing bounding box on
 
 cv2.imshow("original", img)
 cv2.waitKey(1)
@@ -20,18 +21,72 @@ cv2.waitKey(1)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (25, 25), 0)
 _, thresh = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)
-thresh_mean = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 0)
-thresh_gaussian = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 0)
+thresh_mean = cv2.adaptiveThreshold(
+    blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 0
+)
+thresh_gaussian = cv2.adaptiveThreshold(
+    blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 0
+)
 
-cv2.imshow("thresh", thresh)
-cv2.imshow("thresh_mean", thresh_mean)
-cv2.imshow("thresh_gaussian", thresh_gaussian)
-cv2.waitKey(1)
+# cv2.imshow("thresh", thresh)
+# cv2.imshow("thresh_mean", thresh_mean)
+# cv2.imshow("thresh_gaussian", thresh_gaussian)
+# cv2.waitKey(1)
 
-target_img = img.copy()
+canny = cv2.Canny(thresh, 10, 20)
+# cv2.imshow("canny", canny)
+# cv2.waitKey(1)
 
-shape_detector.detect_draw_contours(thresh, target_img, 4)
-shape_detector.detect_draw_contours(thresh, target_img, 6)
+canny = cv2.morphologyEx(canny, cv2.MORPH_DILATE, (15, 15))
+# cv2.imshow("canny dilated", canny)
+# cv2.waitKey(1)
+
+contours, hierarchy = cv2.findContours(
+    canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+)  # RETR_EXTERNAL
+cv2.drawContours(target_img, contours, -1, (0, 0, 255), 3)
+
+print("number of shapes found:", len(contours))
+print("shapes of each shape:", [contours[i].shape for i, _ in enumerate(contours)])
+print("hierarchy", hierarchy)
+
+# visualizing the different contours
+for i, contour in enumerate(contours):
+    print(f"Is {i} a hexagon? {shape_detector.detect_shape(contour, 6)}")
+    temp_img = img.copy()
+    # [contour] to show connected lines, contour (w/o []) to only show points
+    cv2.drawContours(temp_img, [contour], -1, (0, 0, 255), 3)
+
+    # cv2.imshow(f"contour {i}", temp_img)
+    # cv2.waitKey(1)
+
+### drawing bboxes ###
+
+# draw bboxes of all contours in green
+bboxes = utils.generate_bboxes(contours)
+print("all bboxes:", bboxes)
+utils.draw_bboxes(target_img, bboxes, (0, 255, 0), 2)
+
+# draw common bboxes of contours in blue
+common_bboxes, weights = cv2.groupRectangles(bboxes, 1, 0.2)
+print("common bboxes:", common_bboxes)
+utils.draw_bboxes(target_img, common_bboxes, (255, 0, 0), 4)
+
+
+### drawing centeriods ###
+
+# draw centeriods of contours in red
+contour_centeriods = utils.get_contour_centers(contours)
+utils.draw_circles(target_img, contour_centeriods, color=(0, 0, 255))
+
+# draw centeriods of all bboxes in green
+bboxes_centeriods = utils.get_bbox_centers(bboxes)
+utils.draw_circles(target_img, bboxes_centeriods, color=(0, 255, 0))
+
+# draw centeriods of common bboxes in blue
+common_bboxes_centeriods = utils.get_bbox_centers(common_bboxes)
+utils.draw_circles(target_img, common_bboxes_centeriods, color=(255, 0, 0))
+
 
 cv2.imshow("target", target_img)
 cv2.waitKey(-1)
