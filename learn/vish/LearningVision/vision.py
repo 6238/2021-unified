a04977d4e49ee6e78d318bcbb570c219ed8f596c
Vishnu Velayuthan <100023569@mvla.net>
2021-02-06 13:03:04 -0800

    Added some changes

--- learn/vish/LearningVision/vision.py
+++ learn/vish/LearningVision/vision.py
@@ -4,17 +4,19 @@ import matplotlib.pyplot as plt
 
 img_path = "./datasets/2012-retroreflective-targets.jpg"
 
-print(cv2.__version__)
+print(cv2.__version__) 
+
+if(5 == 4):
+    print("hehee")
 
 #Shows normal image
 img = cv2.imread(img_path) #imread returns numpy array
 # cv2.imshow("original image", img) #Shows image
-#cv2.waitKey(-1) keeps it up
+# cv2.waitKey(-1) #keeps it up
 
 #Lets you see specific pixel points
-plt.imshow(img)
+plt.imshow(img) 
 # plt.show()
-
 
 cropped_img = img[137:215, 185:300]
 # print(cropped_img.shape)
@@ -24,7 +26,8 @@ edges = cv2.Canny(cropped_img, 400, 120)
 alpha = 0.5 #Alpha is for contrast
 beta = 10   #Beta is for brightness
 new_img = cv2.addWeighted(cropped_img, alpha, cropped_img, alpha, beta)
-
+print("heelle")
 plt.imshow(new_img)
 plt.show()
+
 
