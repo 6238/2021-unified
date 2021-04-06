f3d207db40bc6ba5cdedf808699a68e841395e8e
Popcorn Penguins <6238@popcornpenguins.com>
2021-04-06 13:47:25 -0700

    created pidControlTest - shreya

---
+++ experimental/individual/shreya/pidControlTest/src/main/java/frc/robot/subsystems/Factory.java
@@ -0,0 +1,69 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import frc.robot.io.Dial;
+import frc.robot.io.Slider;
+import frc.robot.io.ToggleButton;
+
+public class Factory {
+    /**
+     * Creates a CANSparkMax object
+     * 
+     * @param busID the ID of the motor controller
+     * @return the CANSparkMax object
+     */
+    public CANSparkMax getSparkMotor(int busID) {
+        return new CANSparkMax(busID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    }
+
+    /**
+     * Creates a Talon Motor
+     * 
+     * @param busID the ID of the motor controller
+     * @return the TalonSRX
+     */
+    public WPI_TalonSRX getTalonMotor(int busID) {
+        return new WPI_TalonSRX(busID);
+    }
+
+    public DoubleSolenoid getDoubleSolenoid(int forwardID, int reverseID) {
+        return new DoubleSolenoid(forwardID, reverseID);
+    }
+
+    public Slider getSlider(String name, double defaultValue) {
+        return new Slider(name, defaultValue);
+    }
+
+    public Slider getSlider(String name, double defaultValue, double min, double max) {
+        if (Slider.cache.containsKey(name)) {
+            return Slider.cache.get(name);
+        }
+        return new Slider(name, defaultValue, min, max);
+    }
+
+    public Slider getSlider(String name, double defaultValue, int x, int y, int w, int h) {
+        return new Slider(name, defaultValue, x, y, w, h);
+    }
+
+    public Slider getSlider(String name, double defaultValue, double min, double max, int x, int y, int w, int h) {
+        return new Slider(name, defaultValue, min, max, x, y, w, h);
+    }
+
+    public Dial getDial(String name, double defaultValue) {
+        return new Dial(name, defaultValue);
+    }
+
+    public ToggleButton getToggleButton (String name, boolean defaultValue) {
+        return new ToggleButton(name, defaultValue);
+    }
+
+    private final Compressor compressor = new Compressor();
+    public Compressor getCompressor() {
+        return compressor;
+    }
+}
